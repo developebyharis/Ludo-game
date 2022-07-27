4b2ee6e15d52b329a7268f1da4b878d785405d97
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-27 20:09:49 +0500

    Second commit

--- Ludo-game.js
+++ Ludo-game.js
@@ -36,3 +36,178 @@ function Stuck() {
         }
     }
 }
+function changePlayer() {
+    if (num != 6){
+    var text = document.getElementById('player');
+    switch (text.innerText) {
+        case "red": text.innerText = text.style.color = "blue"; break;
+        case "blue": text.innerText = text.style.color = "yellow"; break;
+        case "yellow": text.innerText = text.style.color = "green"; break;
+        case "green": text.innerText = text.style.color = "red"; break;
+    }
+    }
+    var badtext = document.getElementById('badtext');
+    badtext.innerText = "";
+    var dice = document.getElementById('dice');
+    dice.style.backgroundImage = "url(Images/dice.gif)";
+}
+var positions = {
+    redpawn1: 0, redpawn2: 0, redpawn3: 0, redpawn4: 0,
+    bluepawn1: 0, bluepawn2: 0, bluepawn3: 0, bluepawn4: 0,
+    greenpawn1: 0, greenpawn2: 0, greenpawn3: 0, greenpawn4: 0,
+    yellowpawn1: 0, yellowpawn2: 0, yellowpawn3: 0, yellowpawn4: 0
+};
+var onboard = {
+    redpawn1: 0, redpawn2: 0, redpawn3: 0, redpawn4: 0,
+    bluepawn1: 0, bluepawn2: 0, bluepawn3: 0, bluepawn4: 0,
+    greenpawn1: 0, greenpawn2: 0, greenpawn3: 0, greenpawn4: 0,
+    yellowpawn1: 0, yellowpawn2: 0, yellowpawn3: 0, yellowpawn4: 0
+};
+function DontHaveOtherFree() {
+    var text = document.getElementById('player');
+    for (var i = 1; i <=4; i++) {
+        if (onboard[text.innerText + "pawn" + i] == 1 || positions[text.innerText + "pawn" + i]+num>=44) return false;
+    }
+    return true;
+}
+function CheckForWinner() {
+    if (pawnOut[currcolor] == 4) {
+        var dice = document.getElementById("dice");
+        var player = document.getElementById("player");
+        var uselesstext1 = document.getElementById("uselesstext1");
+        var uselesstext2 = document.getElementById("uselesstext2");
+        dice.innerText = "";
+        dice.style.visibility = "hidden";
+        uselesstext1.innerText = "";
+        uselesstext2.innerText = "";
+        player.innerText = "The Winner is the "+currcolor+" player";
+    }
+}
+function stepDown() {
+    var doc = document.getElementById(currcolor + "pawn"+NumOfPaw);
+    var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
+    doc.style.top = (curr+step)+'px';
+    currPos++;
+}
+function stepUp() {
+    var doc = document.getElementById(currpawn);
+    var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
+    doc.style.top = (curr - step) + 'px';
+    currPos++;
+}
+function stepLeft() {
+    var doc = document.getElementById(currpawn);
+    var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+    doc.style.left = (curr - step) + 'px';
+    currPos++;
+}
+function stepRight() {
+    var doc = document.getElementById(currpawn);
+    var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+    doc.style.left = (curr + step) + 'px';
+    currPos++;
+}
+var stepsRed = [];
+var stepsYellow = [];
+var stepsBlue =[];
+var stepsGreen =[];
+function pushSteps(value, steps, count) {
+    for (i = 0; i < count; i++) steps.push(value);
+}
+//Red pawns path
+pushSteps(stepDown,stepsRed,4);
+pushSteps(stepRight, stepsRed,4);
+pushSteps(stepDown, stepsRed,2);
+pushSteps(stepLeft, stepsRed,4);
+pushSteps(stepDown, stepsRed,4);
+pushSteps(stepLeft, stepsRed,2);
+pushSteps(stepUp, stepsRed,4);
+pushSteps(stepLeft, stepsRed,4);
+pushSteps(stepUp, stepsRed,2);
+pushSteps(stepRight, stepsRed,4);
+pushSteps(stepUp, stepsRed,4);
+pushSteps(stepRight, stepsRed,1);
+pushSteps(stepDown, stepsRed,5);
+//Yellow pawns path
+
+pushSteps(stepUp, stepsYellow,4);
+pushSteps(stepLeft, stepsYellow,4);
+pushSteps(stepUp, stepsYellow,2);
+pushSteps(stepRight, stepsYellow,4);
+pushSteps(stepUp, stepsYellow,4);
+pushSteps(stepRight, stepsYellow,2);
+pushSteps(stepDown, stepsYellow,4);
+pushSteps(stepRight, stepsYellow,4);
+pushSteps(stepDown, stepsYellow,2);
+pushSteps(stepLeft, stepsYellow,4);
+pushSteps(stepDown, stepsYellow,4);
+pushSteps(stepLeft, stepsYellow,1);
+pushSteps(stepUp, stepsYellow,5);
+
+//Blue pawns path
+pushSteps(stepLeft, stepsBlue,4);
+pushSteps(stepDown, stepsBlue,4);
+pushSteps(stepLeft, stepsBlue,2);
+pushSteps(stepUp, stepsBlue,4,2);
+pushSteps(stepLeft, stepsBlue,4);
+pushSteps(stepUp, stepsBlue,2);
+pushSteps(stepRight, stepsBlue,4);
+pushSteps(stepUp, stepsBlue,4);
+pushSteps(stepRight, stepsBlue,2);
+pushSteps(stepDown, stepsBlue,4);
+pushSteps(stepRight, stepsBlue,4);
+pushSteps(stepDown, stepsBlue,1);
+pushSteps(stepLeft, stepsBlue,5);
+
+//Green pawns path
+pushSteps(stepRight, stepsGreen,4);
+pushSteps(stepUp, stepsGreen,4);
+pushSteps(stepRight, stepsGreen,2);
+pushSteps(stepDown, stepsGreen,4);
+pushSteps(stepRight, stepsGreen,4);
+pushSteps(stepDown, stepsGreen,2);
+pushSteps(stepLeft, stepsGreen,4);
+pushSteps(stepDown, stepsGreen,4);
+pushSteps(stepLeft, stepsGreen,2);
+pushSteps(stepUp, stepsGreen,4);
+pushSteps(stepLeft, stepsGreen,4);
+pushSteps(stepUp, stepsGreen,1);
+pushSteps(stepRight, stepsGreen, 5);
+function ResetPawn(victim) {
+    onboard[victim] = 0;
+    positions[victim] = 0;
+    var pawnToMove = document.getElementById(victim);
+    switch (victim) {
+        case "redpawn1": pawnToMove.style.top = 149 + "px"; pawnToMove.style.left = 442 + "px"; break;
+        case "redpawn2": pawnToMove.style.top = 102 + "px"; pawnToMove.style.left = 395 + "px"; break;
+        case "redpawn3": pawnToMove.style.top = 55 + "px"; pawnToMove.style.left = 442 + "px"; break;
+        case "redpawn4": pawnToMove.style.top = 102 + "px"; pawnToMove.style.left = 490 + "px"; break;
+        case "bluepawn1": pawnToMove.style.top = 451 + "px"; pawnToMove.style.left = 490 + "px"; break;
+        case "bluepawn2": pawnToMove.style.top = 451 + "px"; pawnToMove.style.left = 395 + "px"; break;
+        case "bluepawn3": pawnToMove.style.top = 404 + "px"; pawnToMove.style.left = 442 + "px"; break;
+        case "bluepawn4": pawnToMove.style.top = 498 + "px"; pawnToMove.style.left = 442 + "px"; break;
+        case "greenpawn1": pawnToMove.style.top = 149 + "px"; pawnToMove.style.left = 93 + "px"; break;
+        case "greenpawn2": pawnToMove.style.top = 102 + "px"; pawnToMove.style.left = 140 + "px"; break;
+        case "greenpawn3": pawnToMove.style.top = 55 + "px"; pawnToMove.style.left = 93 + "px"; break;
+        case "greenpawn4": pawnToMove.style.top = 102 + "px"; pawnToMove.style.left = 47 + "px"; break;
+        case "yellowpawn1": pawnToMove.style.top = 451 + "px"; pawnToMove.style.left = 47 + "px"; break;
+        case "yellowpawn2": pawnToMove.style.top = 451 + "px"; pawnToMove.style.left = 140 + "px"; break;
+        case "yellowpawn3": pawnToMove.style.top = 404 + "px"; pawnToMove.style.left = 93 + "px"; break;
+        case "yellowpawn4": pawnToMove.style.top = 498 + "px"; pawnToMove.style.left = 93 + "px"; break;
+
+    }
+}
+function randomNum() {
+    if (!clicked) {
+        num = Math.floor((Math.random() * 6) + 1);;
+        var dice = document.getElementById('dice');
+        dice.style.backgroundImage = "url(Images/" + num + ".jpg)";
+        clicked = true;
+    }
+    if (num != 6&&DontHaveOtherFree()) {
+        var bad = document.getElementById('badtext');
+        bad.innerText = "Unfortunatlly you stuck";
+        window.setTimeout(changePlayer, 1000);
+        clicked = false;
+    }
+}
